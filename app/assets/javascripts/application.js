58bd399a12e34b2326f255aa0134eef748574958
Raphael <raphaeltnfsh@gmail.com>
2012-12-27 01:30:12 +0800

    Zzzzz

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require .//modernizr.custom.17475
 //= require jquery
 //= require jquery_ujs
 //= require jquery-fileupload
@@ -19,13 +20,9 @@
 //= require chosen-jquery
 //= require fancybox
 //= require raphael
-//= require modernizr.custom.79639
-//= require .//popbox.min.js
 //= require .//facebook
 //= require .//wishPlus
 //= require_tree ../templates/
-//= require .//modernizr.custom.17475
-//= require_tree .//Elastislide_js
 //= require_tree .//models
 //= require_tree .//collections
 //= require_tree .//views
